7bbc53b154d2e964bf0f7979ede8a79246b774fe
Karkusha23 <min.rinat567321@gmail.com>
2024-11-30 17:54:37 +0300

    Update script

--- VideoCaptureServer/static/hlsserver/scripts/hlsclient.js
+++ VideoCaptureServer/static/hlsserver/scripts/hlsclient.js
@@ -1,28 +1,60 @@
+function httpRequest(url)
+{
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", url, false); // false for synchronous request
+  xmlHttp.send(null);
+  return xmlHttp.responseText;
+}
+
 if(Hls.isSupported()) 
 {
-    console.log('123');
-    var video = document.getElementById('video');
-    var data = document.getElementById('data');
-    var hls = new Hls();
-    hls.loadSource('/media_no_cache/playlists/' + data.getAttribute('device') + '/playlist.m3u8');
-    hls.attachMedia(video);
-    hls.on(Hls.Events.MANIFEST_PARSED,function() 
-    {
-       video.play();
-       console.log('event');
-    });
+  console.log('123');
+  var video = document.getElementById('video');
+  var data = document.getElementById('data');
+  var hls = new Hls();
+  var deviceName = data.getAttribute('device')
+  var deviceHeartbeatUrl = '/device/' + deviceName + '/heartbeat';
+  var sourceUrl = '/media_no_cache/playlists/' + deviceName.replaceAll('/', '') + '/playlist.m3u8';
+  var sourceExistsUrl = '/media_exists/playlists/' + deviceName.replaceAll('/', '') + '/playlist.m3u8';
+  var readyToLoad = false
 
-    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-    (async () => {
-      while (true)
+  hls.on(Hls.Events.error, function(event, data)
+  {
+    hlsError = true;
+    console.log('Hls error');
+  })
+
+  hls.on(Hls.Events.MANIFEST_PARSED,function() 
+  {
+    video.play();
+    console.log('event');
+  });
+
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  (async () => {
+    while (true)
       {
         await sleep(2000);
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", '/device_heartbeat/' + data.getAttribute('device'), false); // false for synchronous request
-        xmlHttp.send(null);
+        httpRequest(deviceHeartbeatUrl);
         console.log('Heartbeat');
       }
     })();
+
+  const sleep2 = ms => new Promise(resolve => setTimeout(resolve, ms));
+  (async () => {
+    while (true)
+    {
+      var res = httpRequest(sourceExistsUrl);
+      if (res == '1')
+      {
+        break;
+      }
+      await sleep2(1000);
+    }
+
+    hls.loadSource(sourceUrl);
+    hls.attachMedia(video);
+  })();
 }
 else
 {
