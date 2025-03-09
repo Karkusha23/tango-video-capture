5c6ac5e507c02983188cee2518b2f2ebb03e5866
Karkusha23 <min.rinat567321@gmail.com>
2025-03-09 22:28:54 +0300

    Add video point click event listener

--- VideoCaptureServer/static/hlsserver/scripts/hlsclient.js
+++ VideoCaptureServer/static/hlsserver/scripts/hlsclient.js
@@ -16,7 +16,6 @@ function httpPostRequest(url, body)
 
 if(Hls.isSupported()) 
 {
-  console.log('123');
   var video = document.getElementById('video');
   var data = document.getElementById('data');
   var hls = new Hls();
@@ -25,11 +24,57 @@ if(Hls.isSupported())
   var deviceParamsUrl = '/device/' + deviceName + '/params';
   var sourceUrl = '/media_no_cache/playlists/' + deviceName.replaceAll('/', '') + '/playlist.m3u8';
   var sourceExistsUrl = '/media_exists/playlists/' + deviceName.replaceAll('/', '') + '/playlist.m3u8';
-  var readyToLoad = false
+  var readyToLoad = false;
+  var isFirstFragLoaded = false;
+  var deviceParams = {};
+  var deviceParamsEl = document.getElementById('params');
 
-  var deviceParamEl = document.getElementById('params')
+  var isFullscreen = false
 
-  document.getElementById('post_params').addEventListener('click', function() 
+  video.addEventListener('fullscreenchange', function()
+  {
+    isFullscreen = document.fullscreenElement === video || document.webkitFullscreenElement === video;
+  })
+
+  video.addEventListener('click', function(e)
+  {
+    var rect = e.target.getBoundingClientRect();
+    const styles = window.getComputedStyle(video);
+    var scaleX = 1;
+    var scaleY = 1;
+    var borderLeft = 0;
+    var borderTop = 0;
+    var fullscreenMarginLeft = 0;
+    var fullscreenMarginTop = 0;
+    if (isFullscreen)
+    {
+      var videoRatio = video.width / video.height;
+      var fullscreenRatio = window.innerWidth / window.innerHeight;
+
+      if (videoRatio > fullscreenRatio)
+      {
+        var scale = window.innerWidth / Number(video.getAttribute('width'))
+        fullscreenMarginTop = (window.innerHeight - Number(video.getAttribute('height')) * scale) / 2;
+      }
+      else if (videoRatio < fullscreenRatio)
+      {
+        var scale = window.innerHeight / Number(video.getAttribute('height'))
+        fullscreenMarginLeft = (window.innerWidth - Number(video.getAttribute('width')) * scale) / 2;
+      }
+    }
+    else
+    {
+      var scaleX = parseFloat(styles.transform.split(',')[0].replace('matrix(', '')) || 1;
+      var scaleY = parseFloat(styles.transform.split(',')[3]) || 1;
+      var borderLeft = parseInt(styles.borderLeftWidth, 10) || 0;
+      var borderTop = parseInt(styles.borderTopWidth, 10) || 0;
+    }
+    var x = (e.clientX - rect.left - borderLeft - fullscreenMarginLeft) / scaleX;
+    var y = (e.clientY - rect.top - borderTop - fullscreenMarginTop) / scaleY;
+    console.log(x + ' ' + y)
+  })
+
+  /*document.getElementById('post_params').addEventListener('click', function() 
   { 
     var newDeviceParams = {
       rulerStartX: Number(document.getElementById('ruler_start_x').value) ?? 0,
@@ -40,7 +85,7 @@ if(Hls.isSupported())
       threshold: Number(document.getElementById('threshold').value) ?? 25
     }
     httpPostRequest(deviceParamsUrl, JSON.stringify(newDeviceParams)); 
-  })
+  })*/
 
   hls.on(Hls.Events.error, function(event, data)
   {
@@ -48,10 +93,13 @@ if(Hls.isSupported())
     console.log('Hls error');
   })
 
-  hls.on(Hls.Events.MANIFEST_PARSED, function() 
+  hls.on(Hls.Events.FRAG_LOADED, function() 
   {
-    video.play();
-    console.log('event');
+    if (!isFirstFragLoaded)
+    {
+      video.play()
+      firstFragLoaded = true
+    }
   });
 
   const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -59,7 +107,9 @@ if(Hls.isSupported())
     while (true)
       {
         await sleep(2000);
-        deviceParamEl.innerHTML = httpGetRequest(deviceParamsUrl);
+        var paramsStr = httpGetRequest(deviceParamsUrl)
+        deviceParams = JSON.parse(paramsStr)
+        deviceParamsEl.innerHTML = JSON.stringify(deviceParams);
         console.log('Heartbeat');
       }
     })();
